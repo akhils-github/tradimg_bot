1f54473c426010caa78f1f2afda9a750cda064d3
Akhil S <akhilsomanmeg@gmail.com>
2025-08-17 16:00:48 +0530

    Updated

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def run_bot():
     loop.run_until_complete(application.run_polling())
 
 if __name__ == "__main__":
-    Thread(target=run_bot).start()
+    bot_thread = Thread(target=run_bot, daemon=True)
+    bot_thread.start()
+
     port = int(os.environ.get("PORT", 5000))
     serve(app, host="0.0.0.0", port=port)
