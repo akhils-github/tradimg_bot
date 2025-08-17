9c31b18f226520768788478cf19c7b0e30f85699
Akhil S <akhilsomanmeg@gmail.com>
2025-08-17 15:49:39 +0530

    hb

--- start.py
+++ start.py
@@ -7,16 +7,9 @@ import os
 def run_bot():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-
-    async def start_bot():
-        await application.initialize()
-        await application.start()
-        await application.updater.start_polling()
-
-    loop.run_until_complete(start_bot())
+    loop.run_until_complete(application.run_polling())
 
 if __name__ == "__main__":
     Thread(target=run_bot).start()
-
     port = int(os.environ.get("PORT", 5000))
     serve(app, host="0.0.0.0", port=port)
